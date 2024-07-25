0f2f4481d14b83fa16b26b2c24a025b5d5630020
Your Name <seongsun8330@naver.com>
2024-07-25 20:19:09 +0900

    0725_asdf

--- Assets/02_Scripts/Enemy/EnemyOnDisable.cs
+++ Assets/02_Scripts/Enemy/EnemyOnDisable.cs
@@ -4,9 +4,7 @@ using UnityEngine;
 
 public class EnemyOnDisable : MonoBehaviour
 {
-    private Rigidbody rb;
     public Transform[] Points;
-
 
     void OnEnable() // 켜질 때
     {
@@ -15,35 +13,45 @@ public class EnemyOnDisable : MonoBehaviour
 
     void OnDisable()// 꺼질 때
     {
-        rb.Sleep();
-        rb.velocity = Vector3.zero;
-        gameObject.transform.position = GameObject.Find("EnemyGroup").transform.position;
-        gameObject.SetActive(false);
+        StartDisable();
     }
 
     public void Disable()  // 호출을 위한 메서드
     {
         Invoke("StartDisable", 5.0f);        
     }
+
     void StartDisable()
     {
-        rb.Sleep();
-        rb.velocity = Vector3.zero;
         gameObject.transform.position = GameObject.Find("EnemyGroup").transform.position;
+
         if (gameObject.TryGetComponent<SkeletonDamage>(out var skeletonDamage))
         {
             skeletonDamage.IsDie = false;
             skeletonDamage.capCol.enabled = true;
             skeletonDamage.rb.isKinematic = false;
+            skeletonDamage.rb.Sleep();
+            skeletonDamage.rb.velocity = Vector3.zero;
         }
+
         if (gameObject.TryGetComponent<MonsterDamage>(out var monsterDamage))
         {
             monsterDamage.IsDie = false;
+            monsterDamage.capCol.enabled = true;
+            monsterDamage.rb.isKinematic = false;
+            monsterDamage.rb.Sleep();
+            monsterDamage.rb.velocity = Vector3.zero;
         }
+    
         if (gameObject.TryGetComponent<ZombieDamage>(out var zombieDamage))
         {
             zombieDamage.IsDie = false;
+            zombieDamage.capCol.enabled = true;
+            zombieDamage.rb.isKinematic = false;
+            zombieDamage.rb.Sleep();
+            zombieDamage.rb.velocity = Vector3.zero;
         }
+
         gameObject.SetActive(false);
     }
 }
